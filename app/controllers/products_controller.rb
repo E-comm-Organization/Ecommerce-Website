5a146afbecc746289f97f7aeedcbd7adc5cbfbe7
Arshan <arshankhan9326863763@gmail.com>
2024-10-16 18:01:38 +0530

    Changes In Product, navbar, home etc

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -77,6 +77,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :sku, :size, :price, :mrp, :selling_price, images: [])
+    params.require(:product).permit(:name, :sku, :size, :price, :mrp, :description, :offer, :selling_price, images: [])
   end
 end
---
+++ db/migrate/20241016111350_add_description_and_offer_to_products.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Adding
+class AddDescriptionAndOfferToProducts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :products, :description, :text
+    add_column :products, :offer, :text
+  end
+end

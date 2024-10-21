3389a04161e42df3883944bba5f1f55bf814911a
Arshan <arshankhan9326863763@gmail.com>
2024-10-21 19:35:08 +0530

    Removed Unnecessary Migration files According to Adnan bhai

--- db/migrate/20241016092123_add_description_and_offers_to_products.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Offers field and description field
-class AddDescriptionAndOffersToProducts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :products, :description, :text
-    add_column :products, :offers, :text
-  end
-end

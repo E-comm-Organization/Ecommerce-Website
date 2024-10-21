6db2aab0b8c3668e3b9bb218c16bb445253cdc22
Arshan <arshankhan9326863763@gmail.com>
2024-10-21 18:49:15 +0530

    Now Both UID as well as Provider Is shown

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,14 +5,18 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+      auth_data = request.env['omniauth.auth']
+      puts auth_data.inspect  
 
-      if @user.persisted?
+      @user = User.from_omniauth(auth_data)
+
+      if @user&.persisted?  # Safe navigation to avoid calling persisted? on nil
         sign_in_and_redirect @user, event: :authentication
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n") if @user
+        redirect_to new_user_registration_url, alert: "User could not be created." unless @user  # Handle case where user is nil
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,15 +20,27 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+  data = access_token.info
+  user = User.where(email: data['email']).first
 
-    user ||= User.create(
+  unless user
+    user = User.new(
       email: data['email'],
-      password: Devise.friendly_token[0, 20]
+      password: Devise.friendly_token[0, 20],
+      provider: access_token.provider,
+      uid: access_token.uid 
     )
-    user
+    
+    if user.save
+      puts "User created successfully: #{user.inspect}"  # Debug line
+    else
+      puts "User not created: #{user.errors.full_messages.join(', ')}"  # Debug line
+      return nil  # Return nil if user creation fails
+    end
   end
+  
+  user
+end
 
   private
 

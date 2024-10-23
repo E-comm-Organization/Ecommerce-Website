e89cce39c602a91e1679e440a3bd6f165c59dfe7
Arshan <arshankhan9326863763@gmail.com>
2024-10-23 12:39:58 +0530

    Removed Migration unnecesary

--- db/migrate/20241016111350_add_description_and_offers_to_products.rb
+++
@@ -1,6 +0,0 @@
-class AddDescriptionAndOffersToProducts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :products, :description, :text
-    add_column :products, :offer, :string
-  end
-end
